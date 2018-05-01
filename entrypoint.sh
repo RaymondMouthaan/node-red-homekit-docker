4538918477bfc38714bf8b5af0522bd0551e88c2
Raymond Mouthaan <raymondmmouthaan@gmail.com>
2018-05-01 22:03:05 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+[[ -e /var/run/dbus.pid ]] && su-exec root rm -f /var/run/dbus.pid
+[[ -e /var/run/avahi-daemon/pid ]] && su-exec root rm -f /var/run/avahi-daemon/pid
+[[ -e /var/run/dbus/system_bus_socket ]] && su-exec root rm -f /var/run/dbus/system_bus_socket
+
+echo "Starting dbus-daemon"
+su-exec root dbus-daemon --system --fork
+
+until [ -e /var/run/dbus/system_bus_socket ]; do
+  sleep 1s
+done
+
+echo "Starting Avahi daemon"
+su-exec root avahi-daemon -D --no-chroot -f /etc/avahi/avahi-daemon.conf
+
+npm start -- --userDir /data
