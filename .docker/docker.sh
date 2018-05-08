93811dc39b29cfc06ba828109e54b328ef744cc9
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-05-08 19:36:08 +0200

    disabled python and docker-compose

--- .docker/docker.sh
+++ .docker/docker.sh
@@ -191,12 +191,12 @@ setup_dependencies() {
   echo "PREPARE: Setting up dependencies."
 
   sudo apt update -y
-  sudo apt install realpath python python-pip -y
+  #sudo apt install realpath python python-pip -y
   sudo apt install --only-upgrade docker-ce -y
-  sudo pip install docker-compose || true
+  #sudo pip install docker-compose || true
 
   docker info
-  docker-compose --version
+  #docker-compose --version
 }
 
 update_docker_configuration() {
