b93a12bb817f4992090f45c4ea86f7def075f933
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-05-20 17:00:50 +0200

    - added node-red version in tag - update package.json

--- update_version.sh
+++ update_version.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#export NODE_RED_VERSION=`grep -Eo "\"node-red\": \"(\w*.\w*.\w*)" package.json | cut -d\" -f4`
-#echo $NODE_RED_VERSION
+export NODE_RED_VERSION=`grep -Eo "\"node-red\": \"(\w*.\w*.\w*)" package.json | cut -d\" -f4`
+echo "node-red version: ${NODE_RED_VERSION}."
+sed -i "" -e "s/\(version\": \"\).*\(\"\)/\1$NODE_RED_VERSION\"/g" package.json
 
 export NODE_RED_HOMEKIT_VERSION=`grep -Eo "\"node-red-contrib-homekit-bridged\": \"\^(\w*.\w*.\w*)" package.json | cut -d"^" -f2`
-echo $NODE_RED_HOMEKIT_VERSION
+echo "node-red-contrib-homekit-bridged version: ${NODE_RED_HOMEKIT_VERSION}."
