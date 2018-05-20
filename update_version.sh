74cb2fecbb7bcdc61966f4a625b32245788c1d5b
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-05-20 20:51:45 +0200

    Set global - NODE_RED_VERSION=x.y.z - NODE_RED_HOMEKIT_VERSION=x.y.z

--- update_version.sh
+++ update_version.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export NODE_RED_VERSION=`grep -Eo "\"node-red\": \"(\w*.\w*.\w*)" package.json | cut -d\" -f4`
-echo "node-red version: ${NODE_RED_VERSION}."
+echo "node-red version: ${NODE_RED_VERSION}"
 sed -i "" -e "s/\(version\": \"\).*\(\"\)/\1$NODE_RED_VERSION\"/g" package.json
 
 export NODE_RED_HOMEKIT_VERSION=`grep -Eo "\"node-red-contrib-homekit-bridged\": \"\^(\w*.\w*.\w*)" package.json | cut -d"^" -f2`
-echo "node-red-contrib-homekit-bridged version: ${NODE_RED_HOMEKIT_VERSION}."
+echo "node-red-contrib-homekit-bridged version: ${NODE_RED_HOMEKIT_VERSION}"
