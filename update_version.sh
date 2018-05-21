a7ecbb2d0cff21b1520d1ab5e24f563256a60979
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-05-21 10:03:40 +0200

    cat added

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-ls -al
+cat ./package.json
+
 export NODE_RED_VERSION=`grep -Eo "\"node-red\": \"(\w*.\w*.\w*)" ./package.json | cut -d\" -f4`
 echo "node-red version: ${NODE_RED_VERSION}"
 sed -i "" -e "s/\(version\": \"\).*\(\"\)/\1$NODE_RED_VERSION\"/g" ./package.json
