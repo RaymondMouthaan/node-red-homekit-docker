25e7043405af6a3e2744d8d049d40f34d7bcf9af
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-05-21 10:26:04 +0200

    - update version fixed

--- update_version.sh
+++ update_version.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-cat ./package.json
-
 export NODE_RED_VERSION=`grep -Eo "\"node-red\": \"(\w*.\w*.\w*)" package.json | cut -d\" -f4`
-echo "node-red version: ${NODE_RED_VERSION}"
 sed "s/\(version\": \"\).*\(\"\)/\1$NODE_RED_VERSION\"/g" package.json > tmpfile && mv tmpfile package.json
 
 export NODE_RED_HOMEKIT_VERSION=`grep -Eo "\"node-red-contrib-homekit-bridged\": \"\^(\w*.\w*.\w*)" package.json | cut -d"^" -f2`
-echo "node-red-contrib-homekit-bridged version: ${NODE_RED_HOMEKIT_VERSION}"
 
-cat ./package.json
+echo "### Update version ######################################################"
+echo "node-red version: ${NODE_RED_VERSION}"
+echo "node-red-contrib-homekit-bridged version: ${NODE_RED_HOMEKIT_VERSION}"
+echo "#########################################################################"
