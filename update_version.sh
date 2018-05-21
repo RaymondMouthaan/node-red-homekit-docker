20dacbec191b9af13c6ab1a0a2cd1c59b9cd9b21
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-05-21 10:17:21 +0200

    avoid sed -i

--- update_version.sh
+++ update_version.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cat ./package.json
 
-export NODE_RED_VERSION=`grep -Eo "\"node-red\": \"(\w*.\w*.\w*)" ./package.json | cut -d\" -f4`
+export NODE_RED_VERSION=`grep -Eo "\"node-red\": \"(\w*.\w*.\w*)" package.json | cut -d\" -f4`
 echo "node-red version: ${NODE_RED_VERSION}"
-sed -i "" -e "s/\(version\": \"\).*\(\"\)/\1$NODE_RED_VERSION\"/g" ./package.json
+sed "s/\(version\": \"\).*\(\"\)/\1$NODE_RED_VERSION\"/g" package.json > tmpfile && mv tmpfile package.json
 
-export NODE_RED_HOMEKIT_VERSION=`grep -Eo "\"node-red-contrib-homekit-bridged\": \"\^(\w*.\w*.\w*)" ./package.json | cut -d"^" -f2`
+export NODE_RED_HOMEKIT_VERSION=`grep -Eo "\"node-red-contrib-homekit-bridged\": \"\^(\w*.\w*.\w*)" package.json | cut -d"^" -f2`
 echo "node-red-contrib-homekit-bridged version: ${NODE_RED_HOMEKIT_VERSION}"
+
+cat ./package.json
